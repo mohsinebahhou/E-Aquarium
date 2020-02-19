d6584de25438b399fcd1ff5c7669f205d7211182
Bahhou <mohsinebahhou@gmail.com>
2020-02-19 20:02:09 +0100

    Config Angular with project

--- src/main/java/com/uge/devops/aquarium/EAquarium/controller/ActiviteController.java
+++ src/main/java/com/uge/devops/aquarium/EAquarium/controller/ActiviteController.java
@@ -2,10 +2,12 @@ package com.uge.devops.aquarium.EAquarium.controller;
 
 
 import com.uge.devops.aquarium.EAquarium.models.Activite;
+import com.uge.devops.aquarium.EAquarium.models.Employe;
 import com.uge.devops.aquarium.EAquarium.service.ActiviteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,7 +23,12 @@ public class ActiviteController {
 
     @GetMapping("/Activite/{id}")
     public Activite getActivite(@PathVariable Integer id) {
-        return ActiviteService.getActiviteById(id);
+        Employe e = new Employe(1, "Bahhou", "Mohsine", "mohsine", "123", "Paris", "0456789456" ,"14/11/1993", "7 18 61 00 112 338 23", "Responsable");
+        List<Employe> list = new ArrayList<>();
+        list.add(e);
+        Activite a = new Activite(1,"Activite1","7/02/2020","14:00","18:00",true,1,1,list);
+        //return ActiviteService.getActiviteById(id);
+        return a;
     }
 
 
